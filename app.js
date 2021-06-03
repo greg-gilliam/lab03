08cc11a7e9ac3214eb02ab581ee96161983945ef
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-06-03 16:03:46 -0700

    Added add UI and button listener

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 // import functions
 
 // reference needed DOM elements
-
+const additionInputX = document.getElementById('x');
+const additionInputY = document.getElementById('y');
+const additionButton = document.getElementById('calculate-addition');
+const additionResult = document.getElementById('result-addition');
 
 // set event listeners 
+additionButton.addEventListener('click', () => {
+console.log('eventButton');
+});
   // get user input(s)
   // do any needed work with the value(s)
   // update DOM to reflect new value(s)
